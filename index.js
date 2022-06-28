ff02bd35dad916ac7e689c755dd56d1d0cdeac72
gellany <>
2022-06-28 21:33:47 +0000

    final

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns=require("dns");
+const url=require('url');
+const bodyParser=require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +23,36 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+const short_url=Math.floor((Math.random()*50)+1);
+var inc_url="";
+app.post("/api/shorturl",(req,res)=>{
+   inc_url=req.body.url;
+  var furl=url.parse(inc_url);
+  console.log( url.parse(inc_url));
+   console.log(furl.hostname);
+  dns.lookup(furl.hostname,(err,add,fam)=>{ //dns check
+    if(err){
+      return res.json({error:"invalid url"});
+    }
+    else if(furl.protocol==null||furl.host==null){  //if host and protocol not defined
+     return res.json({
+        error:"invalid url",
+      });
+    }
+    else{
+     return res.json({
+        original_url:inc_url,
+        short_url:short_url
+      })
+    }
+  })
+});
+
+app.get(`/api/shorturl/${short_url}`,(req,res)=>{
+  console.log(`/api/shorturl/${short_url}`);
+  res.redirect(inc_url);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
